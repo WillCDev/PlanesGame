c5e165fe4ce3fdcec8108b2729466483c1ff9d51
Will Chadwick <willchadwick@mac.com>
2017-03-03 23:10:20 +0000

    :tada: Initial Commit

---
+++ js/Planes.js
@@ -0,0 +1,53 @@
+var _Airspace = new function (){
+  var _allPlanes = [];
+
+  return {
+    Planes: function(){ return _allPlanes },
+    newPlane: function(){
+      var x = new _Plane();
+      _allPlanes.push(x);
+      return;
+    },
+    clearPlane: function(num){
+      var obj = _allPlanes[num];
+      clearInterval(obj.timer);
+      _allPlanes.splice(num,1);
+    }
+  }
+}
+
+/***** Plane Constructor Function *****/
+function _Plane(){
+  // var this = this;
+// Plane Objects Variables
+  this._posX = 0;
+  this._posY = 0;
+  this._bearing = 0;
+  this._speed = 10;
+  this._level = 1;
+
+// Plane Objects Built in Methods
+  this.changePosition = function(x,y){
+    this._posX += 1;
+    this._posY += 3;
+    console.log(this._posX + ", " + this._posY);
+    return;
+  };
+  this.changeBearing = function(val){
+    this._bearing = val;
+    return;
+  };
+  this.changeSpeed = function(val){
+    this._speed = val;
+    return;
+  };
+  this.changeLevel = function(val){
+    this._level = val;
+    return;
+  }
+
+// Plane Objects this Invoking Timer Function to update position
+  this.timer = setInterval(this.changePosition.bind(this), 1000);
+
+}
+/***** End of Plan Constructor *****/
